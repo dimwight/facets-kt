8cf0a0df8caa1a8bd44850072d2d1d60683cf2bc
David Wright <dimwight@ntlworld.com>
2018-07-13 15:01:11 +0100

    Debug?

---
+++ src/fkt/facets/index.kt
@@ -0,0 +1,42 @@
+package fkt.facets
+import fkt.java.STarget
+abstract class TargetCoupler {
+  open val targetStateUpdated: ((Any, String) -> Unit)? = null
+}
+abstract class TextualCoupler : TargetCoupler() {
+  open val passText: String? = null
+  open val getText: ((String) -> String)? = null
+  open val isValidText: ((String, String) -> Boolean)? = null
+}
+abstract class TogglingCoupler : TargetCoupler() {
+  abstract val passSet: Boolean
+}
+abstract class NumericCoupler : TargetCoupler() {
+  abstract val passValue: Double
+  open val min: Double? = null
+  open val max: Double? = null
+}
+abstract class IndexingState {
+  abstract val uiSelectables: Array<String>
+  abstract val indexed: Any
+}
+abstract class IndexingFramePolicy {
+  open val indexingTitle: String? = null
+  abstract val getIndexables: () -> (Array<out Any>)
+  open val frameTitle: String? = null
+  open val newUiSelectable: ((Any) -> String)? = null
+  open val newFrameTargets: (() -> (Array<STarget>))? = null
+  open val newIndexedTreeTitle: ((Any) -> String)? = null
+  open val newIndexedTree: ((Any, String) -> STarget)? = null
+}
+abstract class IndexingCoupler : TargetCoupler() {
+  abstract val getIndexables: (String) -> Array<out Any>
+  open val passIndex: Int? = null
+  open val newUiSelectable: ((Any) -> String)? = null
+}
+interface FacetsApp {
+  fun getContentTrees(): Any
+  fun onRetargeted(activeTitle: String)
+  fun buildLayout()
+}
+
