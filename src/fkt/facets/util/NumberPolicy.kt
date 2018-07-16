0bffbccc4a7b31a236b93800ddae5ea5fb3bdaea
David Wright <dimwight@ntlworld.com>
2018-07-16 10:56:35 +0100

    NumberPolicy

--- src/fkt/facets/util/NumberPolicy.kt
+++ src/fkt/facets/util/NumberPolicy.kt
@@ -1,59 +1,13 @@
 package fkt.facets.util
-
-/**
- * Validation and display of numbers.
- *
- * [NumberPolicy] supplies the policy Debug.information required when
- * validating a number or displaying it in the application surface.
- */
-open class NumberPolicy
-/**
- * Construct a policy that constrains valid values within the immutable range
- * `min` to `max`.
- */
-(private val min: Double, private val max: Double) : Tracer("NumberPolicy") {
-  /**
-   * Validation and tick-based display of numbers.
-   *
-   * [Ticked] supplies validation and display policy for
-   * numbers displayed on a scale such as that of a slider.
-   */
-  /**
-   * Convenience constructor for a full-range tick scale.
-   */
-  /**
-   * Convenience constructor for a scale that dynamically defines its constraints.
-   */
-  init {
+class NumberPolicy(private val min: Double, private val max: Double) : Tracer("NumberPolicy") {  init {
     if (min > max) throw IllegalArgumentException("Bad values $min>=$max")
-  }
-
-  /**
-   * The highest possible value under the policy.
-   *
-   * Set immutably during construction but only accessed via
-   * this method, enabling subclasses to define it dynamically by overriding.
-   */
+  }  
   fun max(): Double {
     return max
-  }
-
-  /**
-   * The lowest possible value under the policy.
-   *
-   * Set immutably during construction but only accessed via
-   * this method, enabling subclasses to define it dynamically by overriding.
-   */
+  }  
   fun min(): Double {
     return min
   }
-
-  /**
-   * Returns the nearest valid value to `proposed`.
-   *
-   * Though not used in the basic implementation, specifying `existing`
-   * as a parameter allows for non-linear validation in subclasses.
-   */
   open fun validValue(existing: Double, proposed: Double): Double {
     val min = min()
     val max = max()
@@ -62,17 +16,9 @@ open class NumberPolicy
     val jump = unit() * unitJump()
     val rounded = if (false) Math.rint(adjusted) else Math.rint(adjusted / jump) * jump
     return if (rounded < min) min else if (rounded > max) max else rounded
-  }
-
-  protected fun debug(): Boolean {
+  }  protected fun debug(): Boolean {
     return false
-  }
-
-  /**
-   * The smallest possible increment under the policy.
-   *
-   * The default implementation deduces this value from [.format].
-   */
+  }  
   fun unit(): Double {
     val format = format()
     if (format > FORMAT_DECIMALS_4)
@@ -85,48 +31,19 @@ open class NumberPolicy
       format == FORMAT_DECIMALS_3 -> 0.001
       else -> 0.0001
     }
-  }
-
-  /**
-   * If `true`, values outside the range will be normalised to within
-   * it.
-   *
-   * Default is `false`.
-   */
+  }  
   fun canCycle(): Boolean {
     return false
-  }
-
-  /**
-   * Hint on formatting the number.
-   *
-   * These are defined by the class FORMAT_x constants, which are mutually exclusive.
-   *
-   * For FORMAT_PLACES_x constants
-   * the format should normally be consistent with `unit`.
-   */
+  }  
   fun format(): Int {
     return FORMAT_DECIMALS_0
-  }
-
-  /**
-   * The column width for text boxes in which values are to be displayed.
-   *
-   * Follows AWT convention.
-   */
+  }  
   fun columns(): Int {
     return COLUMNS_DEFAULT
-  }
-
-  /**
-   * Returns the multiple of `unit` defining the current minimum
-   * valid value change.
-   */
+  }  
   open fun unitJump(): Double {
     return UNIT_JUMP_DEFAULT
-  }
-
-  companion object {
+  }  companion object {
     val MIN_VALUE = java.lang.Double.MAX_VALUE * -1
     val MAX_VALUE = java.lang.Double.MAX_VALUE
     var COLUMNS_DEFAULT = 3
