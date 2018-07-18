9e7a2d50fd8323e226600695c9c4c4940a35f97f
David Wright <dimwight@ntlworld.com>
2018-07-18 14:50:48 +0100

    Textual

--- src/fkt/facets/core/Textual.kt
+++ src/fkt/facets/core/Textual.kt
@@ -1,5 +1,19 @@
 package fkt.facets.core
 import fkt.facets.TextualCoupler
+import fkt.facets.util.Debug
+import java.awt.SystemColor.text
+
 class Textual(title:String,coupler: TextualCoupler) : TargetCore(title,coupler){
+  init {
+    state=coupler.passText?:NoState
+  }
+  override fun state():String{
+    val coupler=extra as TextualCoupler
+    return when {
+      state!=NoState -> state as String
+      else -> coupler.getText?.invoke(title)?:throw Error("Missing text in ${Debug.info(this)}")
+    }
+  }
+
 }
 
