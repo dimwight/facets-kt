0683c190f36db4c142a066f9a211e9eb019e2ef1
David Wright <dimwight@ntlworld.com>
2018-07-27 11:52:14 +0100

    app

---
+++ src/fkt/app/FacetsApplet.kt
@@ -0,0 +1,85 @@
+package fkt.app
+
+import fkt.app.TargetTest.*
+import java.awt.GridLayout
+import java.awt.Point
+import java.awt.event.ComponentEvent
+import java.awt.event.ComponentListener
+import javax.swing.BorderFactory.*
+import javax.swing.JApplet
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.border.EtchedBorder
+import fkt.app.SimpleTitles as Titles
+
+/**
+ Superficial host for [fkt.facets.FacetsApp]s
+
+ @param constructor
+
+ @param [args] passed from [main], specify flavour of app
+ */
+class FacetsApplet(private val args: Array<String>) : JApplet() {
+  /**
+  Calls [fkt.AppCore.buildSurface]] on an instance specified by [args]
+   */
+  override fun init() {
+    val content = contentPane as JPanel
+    if(false)content.addComponentListener(object : ComponentListener {
+      override fun componentResized(e: ComponentEvent) {
+        println("componentResized: " + content.size)
+      }
+
+      override fun componentShown(e: ComponentEvent) {}
+      override fun componentMoved(e: ComponentEvent) {}
+      override fun componentHidden(e: ComponentEvent) {}
+    })
+    val simples = TargetTest.simpleValues
+    val style=args.firstOrNull { !it.startsWith("_") }?:""
+    val tests = when (style) {
+        "contenting" -> listOf(Contenting)
+        "selecting" -> listOf(Selecting )
+        else -> if (false) listOf(TargetTest.TogglingLive) else simples
+      }
+    content.layout = GridLayout(if (tests==simples) 3 else 1, 1)
+    for (test in tests) {
+      if (false && !(tests!=simples || test == TogglingLive)) continue
+      val pane = JPanel()
+      pane.border = createCompoundBorder(createEmptyBorder(10, 10, 10, 10),
+        createEtchedBorder(EtchedBorder.LOWERED))
+      content.add(pane)
+      val trace = false
+      when {
+        test.isSimple -> object : SimpleApp(test, trace) {
+          override fun buildLayout() {
+            facets.times.setResetWait(50)
+            facets.times.doTime = false
+            if (test == TogglingLive) {
+              val live = facets.getTargetState(Titles.Toggling) as Boolean
+              facets.setTargetLive(Titles.Toggled, live)
+            }
+            SimpleLayout(pane, this).build()
+          }
+        }
+        test == Contenting -> object : ContentingApp(trace) {
+          override fun buildLayout() = ContentingLayout(pane, this).build()
+        }
+        else -> object : SelectingApp(Selecting, trace) {
+          override fun buildLayout() = SelectingLayout(pane, this).build()
+        }
+      }.buildSurface()
+    }
+  }
+}
+
+fun main(args: Array<String>) {
+  val frame = JFrame("FacetsApplet")
+  frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+  val applet = FacetsApplet(args)
+  frame.contentPane.add(applet)
+  applet.init()
+  if(false)frame.size = applet.minimumSize
+  frame.pack()
+  frame.location=Point(1920,0)
+  javax.swing.SwingUtilities.invokeLater {frame.isVisible = true}
+}
---
+++ src/fkt/app/SelectingLayout.kt
@@ -0,0 +1,49 @@
+package fkt.app
+
+import fkt.swing.PaneLayout
+import fkt.swing.SwingFacet
+import java.awt.CardLayout
+import java.awt.Container
+import java.awt.GridLayout
+import javax.swing.JComponent
+import javax.swing.JPanel
+import fkt.app.SelectingTitles as Titles
+
+open class SelectingLayout(pane: Container, app: SelectingApp) : PaneLayout(pane, app) {
+  protected val cards = CardLayout()
+  protected val cardsParent: JComponent = JPanel(cards)
+  private val listTitle = Titles.Select
+
+  override fun build() {
+    buildCardsBase()
+    pane.layout = GridLayout(2, 1)
+    pane.add(newListFacet(listTitle).mount)
+    pane.add(cardsParent)
+    for ((at, card) in listOf(
+      JPanel(GridLayout(4, 1)),//Short
+      JPanel(GridLayout(4, 1))//Long
+    ).withIndex()) {
+      cardsParent.add(card)
+      val type = SelectableType.values()[at]
+      cards.addLayoutComponent(card, type.toString())
+      val tail = type.titleTail
+      card.add(newTextFieldFacet(Titles.EditText + tail, 20, false).mount)
+      if (type == SelectableType.Long)
+        card.add(newLabelFacet(Titles.CharsCount + tail).mount)
+      card.add(newCheckBoxFacet(Titles.Live).mount)
+    }
+  }
+
+  protected fun buildCardsBase(checkContent: Boolean = true) {
+    object: SwingFacet<JComponent>(cardsParent, facets.activeContentTitle, facets) {
+      override val fieldState = ""
+      override fun addFieldListener() {}
+      override fun updateField(update: Any) {
+        val name = if (!checkContent) update as String
+        else (facets.getIndexingState(listTitle).indexed as TextContent)
+          .selectableType.toString()
+        cards.show(cardsParent, name)
+      }
+    }
+  }
+}
